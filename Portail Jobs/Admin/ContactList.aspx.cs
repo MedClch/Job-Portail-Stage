f504e1ea47d97027187629883a5d5afde1446071
MedClch <chlouchi.med@gmail.com>
2023-07-28 16:09:56 +0100

    xxx

---
+++ Portail Jobs/Admin/ContactList.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Portail_Jobs.Admin
+{
+    public partial class ContactList : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        DataTable dt;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"]!=null)
+                Response.Redirect("../User/Default.aspx");
+            if (Session["admin"]==null)
+                Response.Redirect("../User/Login.aspx");
+            if (Session["admin"]==null && Session["user"]==null)
+                Response.Redirect("../User/Login.aspx");
+            if (!IsPostBack)
+                showContacts();
+        }
+
+        private void showContacts()
+        {
+            string query = string.Empty;
+            conn = new SqlConnection(str);
+            query = @"Select Row_Number() over(Order by (Select 1)) as [Sr.No],ContactId,Name,Email,Subject,Message from Contact";
+            cmd = new SqlCommand(query, conn);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex=e.NewPageIndex;
+            showContacts();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int contactID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                conn = new SqlConnection(str);
+                cmd = new SqlCommand("Delete from Contact where ContactId = @id", conn);
+                cmd.Parameters.AddWithValue("@id", contactID);
+                conn.Open();
+                int r = cmd.ExecuteNonQuery();
+                if (r > 0)
+                {
+                    lblMsg.Text="Contact deleted successfully !";
+                    lblMsg.CssClass="alert alert-success";
+                }
+                else
+                {
+                    lblMsg.Text="Couldn't delete this contact, please try again later !";
+                    lblMsg.CssClass="alert alert-success";
+                }
+                conn.Close();
+                GridView1.EditIndex = -1;
+                showContacts();
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                Response.Write("<script>alert('"+ex.Message+"');</script>");
+            }
+        }
+    }
+}
