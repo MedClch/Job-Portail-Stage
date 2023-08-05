4d252f559b08c74734fbf6d493d66a48ceb560fe
MedClch <chlouchi.med@gmail.com>
2023-08-05 02:07:22 +0100

    xxx

--- Portail Jobs/User/JobDetails.aspx.cs
+++ Portail Jobs/User/JobDetails.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,143 @@ namespace Portail_Jobs.User
 {
     public partial class JobDetails : System.Web.UI.Page
     {
+        SqlConnection conn;
+        SqlCommand cmd;
+        string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        DataTable dt,dt1;
+        SqlDataAdapter adapter;
+        public string jobTitle = string.Empty;
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            if (Request.QueryString["id"]!=null)
+            {
+                showJobDetails();
+                DataBind();
+            }
+            else
+            {
+                Response.Redirect("JobListing.aspx");
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            //showJobDetails();
+        }
 
+        private void showJobDetails()
+        {
+            if (dt==null)
+            {
+                conn = new SqlConnection(str);
+                string query = @"Select * from Jobs where JobId = @id";
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", Request.QueryString["id"]);
+                adapter = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                adapter.Fill(dt);
+            }
+            DataList1.DataSource = dt;
+            DataList1.DataBind();
+            jobTitle = dt.Rows[0]["Title"].ToString();
+        }
+
+        protected void DataList1_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            if(e.CommandName=="ApplyJob")
+            {
+                if (Session["user"]!=null)
+                {
+                    try
+                    {
+                        conn = new SqlConnection(str);
+                        string query = @"Insert into AppliedJobs values (@JobId,@UserId)";
+                        cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@JobId", Request.QueryString["id"]);
+                        cmd.Parameters.AddWithValue("@UserId", Session["userId"]);
+                        conn.Open();
+                        int res = cmd.ExecuteNonQuery();
+                        if (res > 0)
+                        {
+                            lblMsg.Visible = true;
+                            lblMsg.Text = "Job application sent successfully !";
+                            lblMsg.CssClass = "alert alert-success";
+                            showJobDetails();
+                        }
+                        else
+                        {
+                            lblMsg.Visible = true;
+                            lblMsg.Text = "Couldn't apply for this job, please try again !";
+                            lblMsg.CssClass = "alert alert-danger";
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Response.Write("<script>alert('" + ex.Message + "');</script>");
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+                else
+                {
+                    Response.Redirect("Login.aspx");
+                }
+            }
+        }
+
+        protected void DataList1_ItemDataBound(object sender, DataListItemEventArgs e)
+        {
+            if (Session["user"]!=null)
+            {
+                LinkButton btnApplyJob = e.Item.FindControl("lbApplyJob") as LinkButton;
+                if (hasApplied())
+                {
+                    btnApplyJob.Enabled = false;
+                    btnApplyJob.Text = "Applied";
+                }
+                else
+                {
+                    btnApplyJob.Enabled = true;
+                    btnApplyJob.Text = "Apply now";
+                }
+            }
+        }
+
+        protected string GetImageUrl(Object url)
+        {
+            string url1 = "";
+            if (string.IsNullOrEmpty(url.ToString()) || url == DBNull.Value)
+            {
+                url1 = "~/Images/No_image.png";
+            }
+            else
+            {
+                url1 = string.Format("~/{0}", url);
+            }
+            return ResolveUrl(url1);
+        }
+
+        bool hasApplied()
+        {
+            conn = new SqlConnection(str);
+            string query = @"Select * from AppliedJobs where UserId=@UserId and JobId=@JobId";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@UserId", Session["userId"]);
+            cmd.Parameters.AddWithValue("@JobId", Request.QueryString["id"]);
+            adapter = new SqlDataAdapter(cmd);
+            dt1 = new DataTable();
+            adapter.Fill(dt1);
+            if(dt1.Rows.Count == 1)
+            { 
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- Portail Jobs/User/JobDetails.aspx.designer.cs
+++ Portail Jobs/User/JobDetails.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace Portail_Jobs.User
 
     public partial class JobDetails
     {
+
+        /// <summary>
+        /// lblMsg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMsg;
+
+        /// <summary>
+        /// DataList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList DataList1;
     }
 }
