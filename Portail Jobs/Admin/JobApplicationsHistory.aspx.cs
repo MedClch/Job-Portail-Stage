8090dd76b1a90276730a5001eb7902632c79f6a3
MedClch <chlouchi.med@gmail.com>
2023-08-22 00:49:31 +0100

    xxx

--- Portail Jobs/Admin/JobApplicationsHistory.aspx.cs
+++ Portail Jobs/Admin/JobApplicationsHistory.aspx.cs
@@ -83,26 +83,35 @@ namespace Portail_Jobs.Admin
         protected void btnExportToExcel_Click(object sender, EventArgs e)
         {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-
             using (var package = new ExcelPackage())
             {
                 var ws = package.Workbook.Worksheets.Add("Sheet1");
-
                 // Adding headers
+                int columnIndex = 1;
                 for (int i = 0; i < GridView1.Columns.Count; i++)
                 {
-                    ws.Cells[1, i + 1].Value = GridView1.Columns[i].HeaderText;
-                }
-
-                // Adding data
-                for (int i = 0; i < GridView1.Rows.Count; i++)
-                {
-                    for (int j = 0; j < GridView1.Columns.Count; j++)
+                    if (GridView1.Columns[i].HeaderText != "Resume") // Exclude the "Resume" column
                     {
-                        ws.Cells[i + 2, j + 1].Value = GridView1.Rows[i].Cells[j].Text;
+                        ws.Cells[1, columnIndex].Value = GridView1.Columns[i].HeaderText;
+                        columnIndex++;
                     }
                 }
+                DataTable allData = FetchAllData();
 
+                // Adding data from all pages of the GridView
+                for (int i = 0; i < allData.Rows.Count; i++)
+                {
+                    var row = allData.Rows[i];
+                    columnIndex = 1;
+                    for (int j = 0; j < allData.Columns.Count; j++)
+                    {
+                        if (allData.Columns[j].ColumnName != "Resume") // Exclude the "Resume" column
+                        {
+                            ws.Cells[i + 2, columnIndex].Value = row[j];
+                            columnIndex++;
+                        }
+                    }
+                }
                 // Save the Excel package to a MemoryStream
                 using (MemoryStream ms = new MemoryStream())
                 {
@@ -116,6 +125,64 @@ namespace Portail_Jobs.Admin
                 }
             }
         }
+
+        private DataTable FetchAllData()
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                string query = @"SELECT jah.AppliedJobId,j.CompanyName,jah.jobId,j.Title,u.Mobile,u.Name,u.Email,jar.Response from JobApplicationHistory jah
+                            inner join JobApplicationResp jar on jar.AppliedJobId = jah.AppliedJobId
+                            inner join [User] u on jah.UserId = u.UserId
+                            inner join Jobs j on jah.JobId = j.jobId";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            return dataTable;
+        }
+
+        //protected void btnExportToExcel_Click(object sender, EventArgs e)
+        //{
+        //    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+        //    using (var package = new ExcelPackage())
+        //    {
+        //        var ws = package.Workbook.Worksheets.Add("Sheet1");
+
+        //        // Adding headers
+        //        for (int i = 0; i < GridView1.Columns.Count; i++)
+        //        {
+        //            ws.Cells[1, i + 1].Value = GridView1.Columns[i].HeaderText;
+        //        }
+
+        //        // Adding data
+        //        for (int i = 0; i < GridView1.Rows.Count; i++)
+        //        {
+        //            for (int j = 0; j < GridView1.Columns.Count; j++)
+        //            {
+        //                ws.Cells[i + 2, j + 1].Value = GridView1.Rows[i].Cells[j].Text;
+        //            }
+        //        }
+
+        //        // Save the Excel package to a MemoryStream
+        //        using (MemoryStream ms = new MemoryStream())
+        //        {
+        //            package.SaveAs(ms);
+
+        //            Response.Clear();
+        //            Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        //            Response.AddHeader("content-disposition", "attachment; filename=JobApplicationsHistory.xlsx");
+        //            ms.WriteTo(Response.OutputStream);
+        //            Response.End();
+        //        }
+        //    }
+        //}
 
         //protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
         //{
